87a7ad883be8321ce73c1f0612d7f90b0264a4cb
Zeinab Abdulkarim <2370973@bdeb.qc.ca>
2024-05-25 23:25:04 -0400

    ajout bouton acheter

--- zeinab.js
+++ zeinab.js
@@ -1,4 +1,13 @@
-function modifierStyle() {
-    const elemText = document.getElementById("titre");
-    elemText.style.fontFamily= "Comic Sans MS";
+function redirectToBilling() {
+
+    const billingApiUrl = 'https://buy.stripe.com/test_14kaFs9YNgBWe7S001';
+
+    const params = new URLSearchParams({
+     
+        product_id: '12345',
+        user_id: '67890'
+    }).toString();
+
+
+    window.location.href = `${billingApiUrl}?${params}`;
 }
