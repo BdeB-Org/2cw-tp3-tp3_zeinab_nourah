52f6c72aa631f643d887e0bab88acae8cc85fb82
Zeinab Abdulkarim <2370973@bdeb.qc.ca>
2024-05-30 12:13:20 -0400

    ajustement des pages

--- bouton.js
+++ bouton.js
@@ -29,6 +29,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+function payer() {
+
+    const payementUrl = 'https://buy.stripe.com/test_14kaFs9YNgBWe7S001';
+
+    const params = new URLSearchParams({
+        product_id: '12345',
+        user_id: '67890'
+    }).toString();
+
+
+    window.location.href = `${payementUrl}?${params}`;
+}
+
 
 
 
